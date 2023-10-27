8aded5808f1423e1507e46c14dc68318f74cf307
ixsluo <xiaoshan.luo@outlook.com>
2023-10-27 10:02:44 +0800

    feat: add prepare_calypso
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- src/cdakit/main.py
+++ src/cdakit/main.py
@@ -2,10 +2,11 @@ import argparse
 import logging
 
 import cdakit
-import cdakit.log
-import cdakit.prepare_vasp
 import cdakit.find_spg
+import cdakit.log
 import cdakit.match_structure
+import cdakit.prepare_calypso
+import cdakit.prepare_vasp
 
 
 def main(verbose: int, **kwargs):
@@ -25,9 +26,10 @@ def cli():
     # create subparsers
     subparsers = parser.add_subparsers()
     # add subparser to subparsers
-    cdakit.prepare_vasp.add_subparser(subparsers)
     cdakit.find_spg.add_subparser(subparsers)
     cdakit.match_structure.add_subparser(subparsers)
+    cdakit.prepare_calypso.add_subparser(subparsers)
+    cdakit.prepare_vasp.add_subparser(subparsers)
     # parse
     args, unknown_args = parser.parse_known_args()
     # calling main
